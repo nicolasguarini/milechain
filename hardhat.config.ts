d4555d9fe0fac438f00d8c9efc45026c5ea70d37
Nicolas Guarini <nikiguarincia@gmail.com>
2023-03-04 00:30:49 +0100

    Configure networks and envirorment variables

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,58 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy";
+import "dotenv/config";
+import "hardhat-gas-reporter";
+
+const MAINNET_RPC_URL =
+    process.env.MAINNET_RPC_URL ||
+    process.env.ALCHEMY_MAINNET_RPC_URL ||
+    "https://eth-mainnet.alchemyapi.io/v2/your-api-key";
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key";
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
   solidity: "0.8.18",
+  networks: {
+    hardhat: {
+        chainId: 31337,
+    },
+    localhost: {
+        chainId: 31337,
+    },
+    goerli: {
+        url: GOERLI_RPC_URL,
+        accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+        saveDeployments: true,
+        chainId: 5,
+    },
+    mainnet: {
+        url: MAINNET_RPC_URL,
+        accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+        saveDeployments: true,
+        chainId: 1,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+  etherscan: {
+      apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: (process.env.REPORT_GAS) ? true : false,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
 };
+
+export const developmentChains = ["hardhat", "localhost"]
+export const VERIFICATION_BLOCK_CONFIRMATIONS = 6;
 
 export default config;
