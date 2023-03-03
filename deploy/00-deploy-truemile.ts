58a57c839f81822442484d12e129ec2a1e0f8d5f
Nicolas Guarini <nikiguarincia@gmail.com>
2023-03-04 00:37:26 +0100

    Create deploy script

---
+++ deploy/00-deploy-truemile.ts
@@ -0,0 +1,28 @@
+import { DeployFunction } from "hardhat-deploy/dist/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } from "../hardhat.config";
+import verify from "../utils/verify";
+
+const deployContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment){
+    const { deployments, getNamedAccounts, network } = hre;
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    const waitBlockConfirmations: number = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS;
+
+    const contract = await deploy("TrueMile", {
+        from: deployer,
+        args: [],
+        log: true,
+        waitConfirmations: waitBlockConfirmations,
+    });
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...");
+        await verify(contract.address, []);
+    }
+}
+
+export default deployContract;
