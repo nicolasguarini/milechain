fba391900a23035308288baefb70eb94cf137c78
Zeref-zt <lorenzo.ficazzola@outlook.it>
2023-03-07 16:07:06 +0100

    Remove mapping and minor refactoring

--- contracts/TrueMile.sol
+++ contracts/TrueMile.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GNU
 pragma solidity ^0.8.9;
 
- 
 contract TrueMile {
     struct Car {
         string licensePlate;
@@ -9,51 +8,49 @@ contract TrueMile {
         uint256 mileage;
     }
  
-    mapping(string => Car) private carsByLicensePlate; //mappa che associa ad ogni targa un oggetto macchina
-    mapping(string => uint256[]) private mileageRecords; // mappa che associa ad ogni targa il suo registro di km registrati
-    mapping(string => address[]) private ownersByLicensePlate; //mappa che associa a una automobile la lista dei suioi proprietari nel tempo
-    mapping(address => Car[]) private carsByOwner; //mappa che associa ad un proprietario una lista di macchine possedute
- 
+    mapping(string => Car) private cars; 
+    mapping(string => uint256[]) private mileageRecords; 
+    mapping(string => address[]) private ownersRecords; 
+    
     function addCar(string memory licensePlate, uint256 mileage) public {
-        require(carsByLicensePlate[licensePlate].owner == address(0), "Car already exists");
+        require(cars[licensePlate].owner == address(0), "Car already exists");
         Car memory newCar = Car(licensePlate, msg.sender, mileage);
-        carsByLicensePlate[licensePlate] = newCar;
+        cars[licensePlate] = newCar;
         mileageRecords[licensePlate].push(mileage);
-        ownersByLicensePlate[licensePlate].push(msg.sender);
-        carsByOwner[msg.sender].push(newCar);
+        ownersRecords[licensePlate].push(msg.sender);   
     }
  
     function updateMileage(string memory licensePlate, uint256 mileage) public {
         require(
-            carsByLicensePlate[licensePlate].owner == msg.sender,
+            cars[licensePlate].owner == msg.sender,
             "You do not own this car"
         );
         require(
-            mileage > carsByLicensePlate[licensePlate].mileage,
+            mileage > cars[licensePlate].mileage,
             "New mileage must be greater than current mileage"
         );
-        carsByLicensePlate[licensePlate].mileage = mileage;
+        cars[licensePlate].mileage = mileage;
         mileageRecords[licensePlate].push(mileage);
     }
  
     function getCarByLicencePlate(
         string memory licensePlate
     ) public view returns (string memory, address, uint256) {
-        require(carsByLicensePlate[licensePlate].owner != address(0), "Car not found");
+        require(cars[licensePlate].owner != address(0), "Car not found");
         return (
-            carsByLicensePlate[licensePlate].licensePlate,
-            carsByLicensePlate[licensePlate].owner,
-            carsByLicensePlate[licensePlate].mileage
+            cars[licensePlate].licensePlate,
+            cars[licensePlate].owner,
+            cars[licensePlate].mileage
         );
     }
  
     function getLatestMileage(
         string memory licensePlate
     ) public view returns (uint256) {
-        require(carsByLicensePlate[licensePlate].owner != address(0), "Car not found");
+        require(cars[licensePlate].owner != address(0), "Car not found");
         uint256[] memory records = mileageRecords[licensePlate];
         if (records.length == 0) {
-            return carsByLicensePlate[licensePlate].mileage;
+            return cars[licensePlate].mileage;
         } else {
             return records[records.length - 1];
         }
@@ -62,24 +59,20 @@ contract TrueMile {
     function getMileageRecord(
         string memory licensePlate
     ) public view returns (uint256[] memory) {
-        require(carsByLicensePlate[licensePlate].owner != address(0), "Car not found");
+        require(cars[licensePlate].owner != address(0), "Car not found");
         return mileageRecords[licensePlate];
     }
  
     function changeOwner(string memory licensePlate, address newOwner) public {
-        require(carsByLicensePlate[licensePlate].owner != address(0), "Car not found");
-        carsByLicensePlate[licensePlate].owner = newOwner;
-        ownersByLicensePlate[licensePlate].push(newOwner);
+        require(cars[licensePlate].owner != address(0), "Car not found");
+        cars[licensePlate].owner = newOwner;
+        ownersRecords[licensePlate].push(newOwner);
     }
  
     function getOwners(
         string memory licensePlate
     ) public view returns (address[] memory) {
-        require(carsByLicensePlate[licensePlate].owner != address(0), "Car not found");
-        return ownersByLicensePlate[licensePlate];
-    }
- 
-    function getCarsByOwner(address owner) public view returns (Car[] memory) {
-        return carsByOwner[owner];
+        require(cars[licensePlate].owner != address(0), "Car not found");
+        return ownersRecords[licensePlate];
     }
 }
