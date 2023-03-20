5ba6ad4475da6a1a72bbcb3abfb8a8f2affa3e38
Nicolas Guarini <nikiguarincia@gmail.com>
2023-03-20 22:03:36 +0100

    Create unit test for `changeOwner` function

--- test/unit/MileChain.test.ts
+++ test/unit/MileChain.test.ts
@@ -84,8 +84,8 @@ import { MileChain, MileChain__factory } from "../../typechain-types";
         it("updates the miles correctly", async() =>{
             await milechain.addVehicle("AA000AA", 1000);
             await milechain.updateMileage("AA000AA", 10000);
-            const vehicle:MileChain.VehicleStruct = await milechain.getVehicle("AA000AA");
-            const vehicleRecord:MileChain.MileageRecordStruct[] = await milechain.getMileageRecords("AA000AA");
+            const vehicle: MileChain.VehicleStruct = await milechain.getVehicle("AA000AA");
+            const vehicleRecord: MileChain.MileageRecordStruct[] = await milechain.getMileageRecords("AA000AA");
             
             assert.equal(vehicle.mileage, 10000);
             assert.equal(vehicleRecord[0].mileage, 1000);
@@ -114,6 +114,42 @@ import { MileChain, MileChain__factory } from "../../typechain-types";
 
             await expect(
                 milechain.updateMileage("AA000AA", 10000)
+            ).revertedWith("Contract is in read-only mode for security reasons");
+        });
+    });
+
+    describe("changeOwner", function(){
+        it("updates the owner correctly", async () => {
+            await milechain.addVehicle("AA000AA", 1000);
+            await milechain.changeOwner("AA000AA", accounts[1].address);
+            const vehicle: MileChain.VehicleStruct = await milechain.getVehicle("AA000AA");
+            const ownersRecords: MileChain.OwnersRecordStruct[] = await milechain.getOwnersRecords("AA000AA");
+
+            assert.equal(accounts[1].address, vehicle.owner);
+            assert.equal(deployer.address, ownersRecords[0].owner);
+            assert.equal(accounts[1].address, ownersRecords[1].owner);
+        });
+
+        it("reverts if the vehicle does not exists", async () => {
+            await expect(
+                milechain.changeOwner("AA000AA", accounts[1].address)
+            ).revertedWith("Vehicle not found");
+        });
+
+        it("reverts if you are not the owner", async () => {
+            await milechain.addVehicle("AA000AA", 1000);
+            
+            await expect(
+                milechain.connect(accounts[1]).changeOwner("AA000AA", accounts[1].address)
+            ).revertedWith("You do not own this vehicle");
+        });
+
+        it("reverts if the contract is in safe mode", async () => {
+            await milechain.addVehicle("AA000AA", 1000);
+            await milechain.setSafeMode(true);
+
+            await expect(
+                milechain.changeOwner("AA000AA", accounts[1].address)
             ).revertedWith("Contract is in read-only mode for security reasons");
         });
     });
