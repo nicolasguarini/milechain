ed5972e58b0391d3a64558ed43831581ba0f4e0f
Nicolas Guarini <nikiguarincia@gmail.com>
2023-04-03 11:47:23 +0200

    Add db logic to `addVehicle` task

--- tasks/addVehicle.ts
+++ tasks/addVehicle.ts
@@ -1,17 +1,59 @@
 import { task } from "hardhat/config";
 import { MileChain } from "../typechain-types";
 import { address } from "../deployments/localhost/MileChain.json";
+import { developmentChains } from "../hardhat.config";
+import * as mongoDB from "mongodb";
 
 task("addVehicle", "A task to add a new vehicle")
     .addPositionalParam("licensePlate")
     .addPositionalParam("mileage")
     .setAction(async (taskArgs) => {
         const hre = require("hardhat");
-        const milechain: MileChain = await hre.ethers.getContractAt("MileChain", address);
-
         const licensePlate: string = taskArgs.licensePlate;
         const mileage: number = parseInt(taskArgs.mileage);
 
-        await milechain.addVehicle(licensePlate, mileage);
-        console.log("Vehicle added!");
+        if(developmentChains.includes(hre.network.name)){
+            const milechain: MileChain = await hre.ethers.getContractAt("MileChain", address);
+            await milechain.addVehicle(licensePlate, mileage);
+            console.log("Vehicle added!");
+        }else{
+            const networkName = hre.network.name;
+            const contractAddress = require(`../deployments/${networkName}/MileChain.json`).address;
+            const signers = await hre.ethers.getSigners();
+
+            const DB_CONN_STRING: string|undefined = process.env.DB_CONN_STRING;
+            const DB_NAME: string|undefined = process.env.DB_NAME;
+            const DB_FULL_NAME = DB_NAME + "-" + networkName;
+
+            console.log("Connecting to MongoDB Atlas...");
+            const client: mongoDB.MongoClient = new mongoDB.MongoClient(DB_CONN_STRING!);
+            await client.connect();
+            const database = client.db(DB_FULL_NAME);
+
+            try{
+                await database.collection("vehicles").insertOne({
+                    licensePlate: licensePlate,
+                    mileage: mileage,
+                    owner: signers[0].address
+                });
+
+                await database.collection("owners").insertOne({
+                    name: "",
+                    surname: "",
+                    address: signers[0].address
+                });
+
+                console.log("");
+            }catch(e){
+                console.log(e);
+            }
+            
+            try{
+                const myContract: MileChain = await hre.ethers.getContractAt("MileChain", contractAddress, signers[0]);
+                await myContract.addVehicle(licensePlate, mileage);
+                console.log("Added to blockchain");
+            }catch(e){
+                await database.collection("vehicles").deleteOne({licensePlate: licensePlate});
+            }
+        }
     });
