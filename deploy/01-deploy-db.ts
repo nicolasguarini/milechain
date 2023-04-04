8a4c953ee27ea0d060990cb8c1dbf9aeda9a8dfc
Nicolas Guarini <nikiguarincia@gmail.com>
2023-04-04 14:11:35 +0200

    Uninstall prisma

--- deploy/01-deploy-db.ts
+++ deploy/01-deploy-db.ts
@@ -9,31 +9,31 @@ const deployDb: DeployFunction = async function (hre: HardhatRuntimeEnvironment)
     const DB_NAME: string|undefined = process.env.DB_NAME;
 
     if(!developmentChains.includes(network.name) && DB_CONN_STRING && DB_NAME){
-    //     const DB_FULL_NAME = DB_NAME + "-" + network.name;
+        const DB_FULL_NAME = DB_NAME + "-" + network.name;
 
-    //     console.log("Connecting to MongoDB Atlas...");
-    //     const client: mongoDB.MongoClient = new mongoDB.MongoClient(DB_CONN_STRING);
-    //     await client.connect();
-    //     console.log("Connected!");
+        console.log("Connecting to MongoDB Atlas...");
+        const client: mongoDB.MongoClient = new mongoDB.MongoClient(DB_CONN_STRING);
+        await client.connect();
+        console.log("Connected!");
         
-    //     const dbList: mongoDB.ListDatabasesResult = await client.db().admin().listDatabases();
-    //     const database = client.db(DB_FULL_NAME);
+        const dbList: mongoDB.ListDatabasesResult = await client.db().admin().listDatabases();
+        const database = client.db(DB_FULL_NAME);
 
-    //     if(dbList.databases.find((db) => db.name == DB_FULL_NAME) === undefined){
-    //         console.log("Creating " + DB_FULL_NAME + " database...");
-    //     }else{
-    //         console.log(DB_FULL_NAME + " already exists, dropping and recreating it."); //we could duplicate the existing db and dropping the duplicate
-    //         await database.dropDatabase();
-    //     }
+        if(dbList.databases.find((db) => db.name == DB_FULL_NAME) === undefined){
+            console.log("Creating " + DB_FULL_NAME + " database...");
+        }else{
+            console.log(DB_FULL_NAME + " already exists, dropping and recreating it."); //we could duplicate the existing db and dropping the duplicate
+            await database.dropDatabase();
+        }
 
-    //     try{
-    //         await database.createCollection("vehicles");
-    //         await database.createCollection("owners");
-    //         console.log(DB_FULL_NAME + " created!");
-    //     }finally{
-    //         client.close();
-    //         console.log("Client closed.")
-    //     }
+        try{
+            await database.createCollection("vehicles");
+            await database.createCollection("owners");
+            console.log(DB_FULL_NAME + " created!");
+        }finally{
+            client.close();
+            console.log("Client closed.")
+        }
     }
 }
 
