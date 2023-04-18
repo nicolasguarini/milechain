58eccbace29a4678ad0f1fcd8d491c031d59bd54
Nicolas Guarini <nikiguarincia@gmail.com>
2023-04-18 14:05:33 +0200

    Create fetch logic from API

--- frontend/pages/search.tsx
+++ frontend/pages/search.tsx
@@ -1,51 +1,48 @@
 import Container from "@/components/container";
 import Layout from "@/components/layout";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Search(){
     const router = useRouter();
+    const [data, setData]: any[] = useState([])
+    const [isLoading, setLoading] = useState(false)
+
+    useEffect(()=>{
+        setLoading(true)
+        if(!router.isReady) return;
+        const url = `http://localhost:8888/.netlify/functions/searchVehicles?network=sepolia&query=${router.query.content}`
+
+        fetch(url, {method: "GET"})
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data.vehicles)
+                setLoading(false)
+            });
+    }, [router.isReady]);
+
+
     return (
         <Layout>
             <Container>
                 <div className="w-full mb-10">
                     <h1 className="text-center text-4xl font-bold p-5">Search: {router.query.content}</h1>
                 </div>
-                <div className="">
-                    <div className=" border-b-2 border-primary border-opacity-50 pb-6">
-                        <div className="text-xl font-bold py-4">DW454DW</div>
-                        <div>Current mileage: 1000km</div>
-                        <div>Owner: 0x23423049380</div>
-                        <div>Last update: 10/04/2023</div>
-                    </div>
 
-                    <div className=" border-b-2 border-primary border-opacity-50 pb-6">
-                        <div className="text-xl font-bold py-4">DW454DW</div>
-                        <div>Current mileage: 1000km</div>
-                        <div>Owner: 0x23423049380</div>
-                        <div>Last update: 10/04/2023</div>
+                {isLoading ? <div>Loading...</div> : (
+                    <div className="">
+                        {data.map((vehicle: any) => {
+                            return (
+                                <div className=" border-b-2 border-primary border-opacity-50 pb-6">
+                                    <div className="text-xl font-bold py-4">{vehicle.licensePlate}</div>
+                                    <div>Current mileage: {vehicle.mileage}</div>
+                                    <div>Owner: {vehicle.owner}</div>
+                                </div>
+                            )
+                        })}
+                    
                     </div>
-
-                    <div className=" border-b-2 border-primary border-opacity-50 pb-6">
-                        <div className="text-xl font-bold py-4">DW454DW</div>
-                        <div>Current mileage: 1000km</div>
-                        <div>Owner: 0x23423049380</div>
-                        <div>Last update: 10/04/2023</div>
-                    </div>
-
-                    <div className=" border-b-2 border-primary border-opacity-50 pb-6">
-                        <div className="text-xl font-bold py-4">DW454DW</div>
-                        <div>Current mileage: 1000km</div>
-                        <div>Owner: 0x23423049380</div>
-                        <div>Last update: 10/04/2023</div>
-                    </div>
-
-                    <div className=" border-b-2 border-primary border-opacity-50 pb-6">
-                        <div className="text-xl font-bold py-4">DW454DW</div>
-                        <div>Current mileage: 1000km</div>
-                        <div>Owner: 0x23423049380</div>
-                        <div>Last update: 10/04/2023</div>
-                    </div>
-                </div>
+                )}
             </Container>
         </Layout>
     )
