98c476fabdffe6b85ec9fa75bf3694e33cda5a37
Nicolas Guarini <nikiguarincia@gmail.com>
2023-04-18 18:08:26 +0200

    Enable Web3 Wallet Authentication

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'flowbite';
+import { MoralisProvider } from 'react-moralis';
+import { NotificationProvider } from 'web3uikit';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider initializeOnMount={false}>
+      <NotificationProvider>
+        <Component {...pageProps} />
+      </NotificationProvider>
+    </MoralisProvider>
+  )
 }
