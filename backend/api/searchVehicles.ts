3c2620ac90d48ae5b82ae4cf7d422a046c577e2a
Zeref <lorenzo.ficazzola@outlook.it>
2023-04-18 13:07:36 +0200

    add apli for search vehicles from licenseplate

---
+++ backend/api/searchVehicles.ts
@@ -0,0 +1,42 @@
+import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+import { Db } from "mongodb";
+import { MongoClient } from "mongodb";
+
+const mongoClient: MongoClient = new MongoClient(process.env.DB_CONN_STRING!);
+const clientPromise = mongoClient.connect();
+
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+    if(event.httpMethod == "GET"){
+        const network: string = event.queryStringParameters!.network!;
+        const query: string = event.queryStringParameters!.query!;
+        try{
+            if(network == "sepolia" || network == "mainnet"){
+                const database: Db = (await clientPromise).db(`milechain-${network}`);
+                const vehicles: any[] = await database.collection("vehicles").find({"licenseplate":query}).toArray();
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({ vehicles: vehicles }),
+                    headers: {"access-control-allow-origin": "*"}
+                };
+            }else{
+                return {
+                    statusCode: 400,
+                    body: JSON.stringify({ message: "Invalid network"}),
+                    headers: {"access-control-allow-origin": "*"}
+                };
+            }
+        }catch(e){
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ message: "DB Error" }),
+                headers: {"access-control-allow-origin": "*"}
+            };
+        }
+    }else{
+        return {
+            statusCode: 405,
+            body: JSON.stringify({ message: "Method not allowed"}),
+            headers: {"access-control-allow-origin": "*"}
+        }
+    }                
+    }
