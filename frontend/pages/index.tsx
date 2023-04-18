1f48a1779d2241c6ec7c50724059ff5bc4bd8962
Nicolas Guarini <nikiguarincia@gmail.com>
2023-04-18 11:13:37 +0200

    Update search bar layout

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,6 @@
 import Layout from '@/components/layout'
 import OwnersCount from '@/components/ownersCount'
+import SearchBar from '@/components/searchBar'
 import VehiclesCount from '@/components/vehiclesCount'
 
 export default function Home() {
@@ -9,11 +10,9 @@ export default function Home() {
         <h1 className={`text-4xl font-bold text-center max-w-5xl mb-3`}>DApp for registering vehicles mileages and ownership history on Ethereum blockchain.</h1>
         <p className='text-primary-darker opacity-50 mb-10'>Built with Solidity, Alchemy, IPFS and other hot technologies :)</p>
 
-        <input type="text" className='bg-darker border-2 rounded-full border-primary-darker border-opacity-50 w-full max-w-md px-5 py-3 mb-3 font-bold placeholder-primary-darker placeholder-opacity-50' placeholder='Start browsing license plates...'/>
-        <div className='flex flex-row w-full max-w-md justify-between text-primary-darker opacity-50 text-sm'>
-          <VehiclesCount />
-          <OwnersCount />
-        </div>
+        {/* <input type="text" className='bg-darker border-2 rounded-full border-primary-darker border-opacity-50 w-full max-w-md px-5 py-3 mb-3 font-bold placeholder-primary-darker placeholder-opacity-50' placeholder='Start browsing license plates...'/> */}
+        <SearchBar />
+
       </main>
     </Layout>                 
   )
