8a881560e60adaa7a24af29868b9fd4f7f28e489
Nicolas Guarini <nikiguarincia@gmail.com>
2023-04-19 15:46:43 +0200

    Remove Web3UIKit

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,14 +2,11 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'flowbite';
 import { MoralisProvider } from 'react-moralis';
-import { NotificationProvider } from 'web3uikit';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MoralisProvider initializeOnMount={false}>
-      <NotificationProvider>
-        <Component {...pageProps} />
-      </NotificationProvider>
+      <Component {...pageProps} />
     </MoralisProvider>
   )
 }
