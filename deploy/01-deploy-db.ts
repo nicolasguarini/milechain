6adb14d0c5ace0593d90dc71e835d0d99ea58c80
Nicolas Guarini <nikiguarincia@gmail.com>
2023-04-22 19:53:40 +0200

    Minor refactoring

--- deploy/01-deploy-db.ts
+++ deploy/01-deploy-db.ts
@@ -4,34 +4,37 @@ import { developmentChains } from "../hardhat.config";
 import * as mongoDB from "mongodb";
 import MongoDatabase from "../utils/db";
 
-const deployDb: DeployFunction = async function (hre: HardhatRuntimeEnvironment){
-    const { network } = hre;
-    const DB_CONN_STRING: string|undefined = process.env.DB_CONN_STRING;
-    const DB_NAME: string|undefined = process.env.DB_NAME;
+const deployDb: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment
+) {
+  const { network } = hre;
+  const DB_CONN_STRING: string | undefined = process.env.DB_CONN_STRING;
 
-    if(!developmentChains.includes(network.name) && DB_CONN_STRING && DB_NAME){
-        const DB_NAME = "milechain-" + network.name;
-        const dbList: mongoDB.ListDatabasesResult = await MongoDatabase
-            .getInstance()
-            .getClient()
-            .db()
-            .admin()
-            .listDatabases();
+  if (!developmentChains.includes(network.name) && DB_CONN_STRING) {
+    const dbName = "milechain-" + network.name;
+    const dbList: mongoDB.ListDatabasesResult =
+      await MongoDatabase.getInstance()
+        .getClient()
+        .db()
+        .admin()
+        .listDatabases();
 
-        const database = MongoDatabase.getInstance().getClient().db(DB_NAME);
+    const database = MongoDatabase.getInstance().getClient().db(dbName);
 
-        if(dbList.databases.find((db) => db.name == DB_NAME) !== undefined){
-            console.log(DB_NAME + " already exists, dropping and recreating it."); //we could duplicate the existing db and dropping the duplicate
-            await database.dropDatabase();
-        }
-
-        try{
-            await database.createCollection("vehicles");
-            await database.createCollection("owners");
-            console.log(DB_NAME + " created!");
-        }catch(e){console.error(e);}
+    if (dbList.databases.find((db) => db.name == dbName) !== undefined) {
+      console.log(dbName + " already exists, dropping and recreating it."); //we could duplicate the existing db and dropping the duplicate
+      await database.dropDatabase();
     }
-}
+
+    try {
+      await database.createCollection("vehicles");
+      await database.createCollection("owners");
+      console.log(dbName + " created!");
+    } catch (e) {
+      console.error(e);
+    }
+  }
+};
 
 export default deployDb;
 deployDb.tags = ["all", "db"];
