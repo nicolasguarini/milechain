788d48e5a14ff90e15d01b22f3172867b3db9422
Nicolas Guarini <nikiguarincia@gmail.com>
2023-05-08 16:24:46 +0200

    Update unit tests for handling reverts on custom errors

--- test/unit/MileChain.test.ts
+++ test/unit/MileChain.test.ts
@@ -4,160 +4,181 @@ import { assert, expect } from "chai";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { MileChain, MileChain__factory } from "../../typechain-types";
 
-!developmentChains.includes(network.name) ? describe.skip : describe("MileChain Unit Tests", function () {
-    let accounts: SignerWithAddress[];
-    let deployer: SignerWithAddress;
-    let secondDeployer: SignerWithAddress;
-    let milechainFactory: MileChain__factory;
-    let milechain: MileChain;
+!developmentChains.includes(network.name)
+  ? describe.skip
+  : describe("MileChain Unit Tests", function () {
+      let accounts: SignerWithAddress[];
+      let deployer: SignerWithAddress;
+      let secondDeployer: SignerWithAddress;
+      let milechainFactory: MileChain__factory;
+      let milechain: MileChain;
 
-    beforeEach(async () => {
+      beforeEach(async () => {
         accounts = await ethers.getSigners();
         deployer = accounts[0];
         secondDeployer = accounts[1];
-        
+
         await deployments.fixture(["milechain"]);
         milechainFactory = await ethers.getContractFactory("MileChain");
         milechain = await milechainFactory.deploy([secondDeployer.address]);
-    });
+      });
 
-    describe("addVehicle", function(){
+      describe("addVehicle", function () {
         it("saves a new vehicle", async () => {
-            await milechain.addVehicle("AA000AA", 1000);
-            const vehicle: MileChain.VehicleStruct = await milechain.getVehicle("AA000AA");
-            const { licensePlate, mileage, owner } = vehicle;
+          await milechain.addVehicle("AA000AA", 1000);
+          const vehicle: MileChain.VehicleStruct = await milechain.getVehicle(
+            "AA000AA"
+          );
+          const { licensePlate, mileage, owner } = vehicle;
 
-            assert.equal("AA000AA", licensePlate);
-            assert.equal(1000, mileage);
-            assert.equal(deployer.address, owner);
+          assert.equal("AA000AA", licensePlate);
+          assert.equal(1000, mileage);
+          assert.equal(deployer.address, owner);
         });
 
         it("updates mileage records", async () => {
-            await milechain.addVehicle("AA000AA", 1000);
-            const mileageRecord: MileChain.MileageRecordStruct[] = await milechain.getMileageRecords("AA000AA");
+          await milechain.addVehicle("AA000AA", 1000);
+          const mileageRecord: MileChain.MileageRecordStruct[] =
+            await milechain.getMileageRecords("AA000AA");
 
-            assert.equal(1000, mileageRecord[0].mileage);
+          assert.equal(1000, mileageRecord[0].mileage);
         });
-        
-        it("updates owners record", async () => {
-            await milechain.addVehicle("AA000AA", 1000);
-            const ownersRecord: MileChain.OwnersRecordStruct[] = await milechain.getOwnersRecords("AA000AA");
 
-            assert.equal(deployer.address, ownersRecord[0].owner);
+        it("updates owners record", async () => {
+          await milechain.addVehicle("AA000AA", 1000);
+          const ownersRecord: MileChain.OwnersRecordStruct[] =
+            await milechain.getOwnersRecords("AA000AA");
+
+          assert.equal(deployer.address, ownersRecord[0].owner);
         });
 
         it("reverts if vehicle already exists", async () => {
-            await milechain.addVehicle("AA000AA", 1000);
+          await milechain.addVehicle("AA000AA", 1000);
 
-            await expect(
-                milechain.addVehicle("AA000AA", 1000)
-            ).revertedWith("Vehicle already exists");
+          await expect(
+            milechain.addVehicle("AA000AA", 1000)
+          ).revertedWithCustomError(milechain, "VehicleAlreadyExists");
         });
 
         it("reverts if contract is in safe mode", async () => {
-            await milechain.setSafeMode(true);
+          await milechain.setSafeMode(true);
 
-            await expect(
-                milechain.addVehicle("AA000AA", 1000)
-            ).revertedWith("Contract is in read-only mode for security reasons");
+          await expect(
+            milechain.addVehicle("AA000AA", 1000)
+          ).revertedWithCustomError(milechain, "SafeModeEnabled");
         });
-    });
+      });
 
-    describe("updateMileage", function(){
-        it("updates the miles correctly", async() =>{
-            await milechain.addVehicle("AA000AA", 1000);
-            await milechain.updateMileage("AA000AA", 10000);
-            const vehicle: MileChain.VehicleStruct = await milechain.getVehicle("AA000AA");
-            const vehicleRecord: MileChain.MileageRecordStruct[] = await milechain.getMileageRecords("AA000AA");
-            
-            assert.equal(vehicle.mileage, 10000);
-            assert.equal(vehicleRecord[0].mileage, 1000);
-            assert.equal(vehicleRecord[1].mileage, 10000);
-        });
+      describe("updateMileage", function () {
+        it("updates the miles correctly", async () => {
+          await milechain.addVehicle("AA000AA", 1000);
+          await milechain.updateMileage("AA000AA", 10000);
+          const vehicle: MileChain.VehicleStruct = await milechain.getVehicle(
+            "AA000AA"
+          );
+          const vehicleRecord: MileChain.MileageRecordStruct[] =
+            await milechain.getMileageRecords("AA000AA");
 
-        it("reverts if you are not the owner", async() => {
-            await milechain.addVehicle("AA000AA", 1000);
-
-            await expect(
-                milechain.connect(accounts[1]).updateMileage("AA000AA", 2000)
-            ).revertedWith("You do not own this vehicle");
+          assert.equal(vehicle.mileage, 10000);
+          assert.equal(vehicleRecord[0].mileage, 1000);
+          assert.equal(vehicleRecord[1].mileage, 10000);
         });
 
-        it("reverts if the mileage is lower than current mileage", async() =>{
-            await milechain.addVehicle("AA000AA", 1000);
-
-            await expect(
-                milechain.updateMileage("AA000AA", 100)
-            ).revertedWith("New mileage must be greater than current mileage");
-        });
-
-        it("reverts if contract is in safe mode",async () => {
-            await milechain.addVehicle("AA000AA", 1000);
-            await milechain.setSafeMode(true);
-
-            await expect(
-                milechain.updateMileage("AA000AA", 10000)
-            ).revertedWith("Contract is in read-only mode for security reasons");
-        });
-    });
-
-    describe("changeOwner", function(){
-        it("updates the owner correctly", async () => {
-            await milechain.addVehicle("AA000AA", 1000);
-            await milechain.changeOwner("AA000AA", accounts[1].address);
-            const vehicle: MileChain.VehicleStruct = await milechain.getVehicle("AA000AA");
-            const ownersRecords: MileChain.OwnersRecordStruct[] = await milechain.getOwnersRecords("AA000AA");
-
-            assert.equal(accounts[1].address, vehicle.owner);
-            assert.equal(deployer.address, ownersRecords[0].owner);
-            assert.equal(accounts[1].address, ownersRecords[1].owner);
-        });
-
-        it("reverts if the vehicle does not exists", async () => {
-            await expect(
-                milechain.changeOwner("AA000AA", accounts[1].address)
-            ).revertedWith("Vehicle not found");
+        it("reverts if vehicle does not exists", async () => {
+          await expect(
+            milechain.updateMileage("AA000AA", 1200)
+          ).revertedWithCustomError(milechain, "VehicleNotFound");
         });
 
         it("reverts if you are not the owner", async () => {
-            await milechain.addVehicle("AA000AA", 1000);
-            
-            await expect(
-                milechain.connect(accounts[1]).changeOwner("AA000AA", accounts[1].address)
-            ).revertedWith("You do not own this vehicle");
+          await milechain.addVehicle("AA000AA", 1000);
+
+          await expect(
+            milechain.connect(accounts[1]).updateMileage("AA000AA", 2000)
+          ).revertedWithCustomError(milechain, "VehicleOfAnotherOwner");
+        });
+
+        it("reverts if the mileage is lower than current mileage", async () => {
+          await milechain.addVehicle("AA000AA", 1000);
+
+          await expect(
+            milechain.updateMileage("AA000AA", 100)
+          ).revertedWithCustomError(milechain, "InvalidMileage");
+        });
+
+        it("reverts if contract is in safe mode", async () => {
+          await milechain.addVehicle("AA000AA", 1000);
+          await milechain.setSafeMode(true);
+
+          await expect(
+            milechain.updateMileage("AA000AA", 10000)
+          ).revertedWithCustomError(milechain, "SafeModeEnabled");
+        });
+      });
+
+      describe("changeOwner", function () {
+        it("updates the owner correctly", async () => {
+          await milechain.addVehicle("AA000AA", 1000);
+          await milechain.changeOwner("AA000AA", accounts[1].address);
+          const vehicle: MileChain.VehicleStruct = await milechain.getVehicle(
+            "AA000AA"
+          );
+          const ownersRecords: MileChain.OwnersRecordStruct[] =
+            await milechain.getOwnersRecords("AA000AA");
+
+          assert.equal(accounts[1].address, vehicle.owner);
+          assert.equal(deployer.address, ownersRecords[0].owner);
+          assert.equal(accounts[1].address, ownersRecords[1].owner);
+        });
+
+        it("reverts if the vehicle does not exists", async () => {
+          await expect(
+            milechain.changeOwner("AA000AA", accounts[1].address)
+          ).revertedWithCustomError(milechain, "VehicleNotFound");
+        });
+
+        it("reverts if you are not the owner", async () => {
+          await milechain.addVehicle("AA000AA", 1000);
+
+          await expect(
+            milechain
+              .connect(accounts[1])
+              .changeOwner("AA000AA", accounts[1].address)
+          ).revertedWithCustomError(milechain, "VehicleOfAnotherOwner");
         });
 
         it("reverts if the contract is in safe mode", async () => {
-            await milechain.addVehicle("AA000AA", 1000);
-            await milechain.setSafeMode(true);
+          await milechain.addVehicle("AA000AA", 1000);
+          await milechain.setSafeMode(true);
 
-            await expect(
-                milechain.changeOwner("AA000AA", accounts[1].address)
-            ).revertedWith("Contract is in read-only mode for security reasons");
+          await expect(
+            milechain.changeOwner("AA000AA", accounts[1].address)
+          ).revertedWithCustomError(milechain, "SafeModeEnabled");
         });
-    });
+      });
 
-    describe("getVehicle", function(){
+      describe("getVehicle", function () {
         it("reverts if vehicle does not exists", async () => {
-            await expect(
-                milechain.getVehicle("AA000AA")
-            ).revertedWith("Vehicle not found");
+          await expect(milechain.getVehicle("AA000AA")).revertedWithCustomError(
+            milechain,
+            "VehicleNotFound"
+          );
         });
-    });
+      });
 
-    describe("getMileageRecords", function(){
+      describe("getMileageRecords", function () {
         it("reverts if vehicle does not exists", async () => {
-            await expect(
-                milechain.getMileageRecords("AA000AA")
-            ).revertedWith("Vehicle not found");
+          await expect(
+            milechain.getMileageRecords("AA000AA")
+          ).revertedWithCustomError(milechain, "VehicleNotFound");
         });
-    });
+      });
 
-    describe("getOwnersRecords", function(){
+      describe("getOwnersRecords", function () {
         it("reverts if vehicle does not exists", async () => {
-            await expect(
-                milechain.getOwnersRecords("AA000AA")
-            ).revertedWith("Vehicle not found");
+          await expect(
+            milechain.getOwnersRecords("AA000AA")
+          ).revertedWithCustomError(milechain, "VehicleNotFound");
         });
+      });
     });
-});
--- test/unit/Owned.test.ts
+++ test/unit/Owned.test.ts
@@ -4,14 +4,16 @@ import { assert, expect } from "chai";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { Owned, Owned__factory } from "../../typechain-types";
 
-!developmentChains.includes(network.name) ? describe.skip : describe("Owned Unit Tests", function () {
-    let accounts: SignerWithAddress[];
-    let deployer: SignerWithAddress;
-    let secondDeployer: SignerWithAddress;
-    let ownedFactory: Owned__factory;
-    let owned: Owned;
-    
-    beforeEach(async () => {
+!developmentChains.includes(network.name)
+  ? describe.skip
+  : describe("Owned Unit Tests", function () {
+      let accounts: SignerWithAddress[];
+      let deployer: SignerWithAddress;
+      let secondDeployer: SignerWithAddress;
+      let ownedFactory: Owned__factory;
+      let owned: Owned;
+
+      beforeEach(async () => {
         accounts = await ethers.getSigners();
         deployer = accounts[0];
         secondDeployer = accounts[1];
@@ -19,84 +21,88 @@ import { Owned, Owned__factory } from "../../typechain-types";
         await deployments.fixture(["milechain"]);
         ownedFactory = await ethers.getContractFactory("Owned");
         owned = await ownedFactory.deploy([secondDeployer.address]);
-    });
+      });
 
-    describe("constructor", function(){
+      describe("constructor", function () {
         it("initializes the contract correctly", async () => {
-            const safeModeState: boolean = await owned.getCurrentSafeModeState();
-            assert.equal(safeModeState, false);
+          const safeModeState: boolean = await owned.getCurrentSafeModeState();
+          assert.equal(safeModeState, false);
         });
 
         it("saves the deployers addresses in deployers mapping", async () => {
-            const isDeployer1 = await owned.isDeployer(deployer.address);
-            const isDeployer2 = await owned.isDeployer(secondDeployer.address);
-            const isDeployer3 = await owned.isDeployer(accounts[2].address);
+          const isDeployer1 = await owned.isDeployer(deployer.address);
+          const isDeployer2 = await owned.isDeployer(secondDeployer.address);
+          const isDeployer3 = await owned.isDeployer(accounts[2].address);
 
-            assert.equal(isDeployer1, true);
-            assert.equal(isDeployer2, true);
-            assert.equal(isDeployer3, false);
+          assert.equal(isDeployer1, true);
+          assert.equal(isDeployer2, true);
+          assert.equal(isDeployer3, false);
         });
-    });
+      });
 
-    describe("setSafeMode", function(){
+      describe("setSafeMode", function () {
         it("enables/disables safe mode correctly", async () => {
-            await owned.setSafeMode(true);
-            const safeModeOn: boolean = await owned.getCurrentSafeModeState();
-            await owned.setSafeMode(false);
-            const safeModeOff: boolean = await owned.getCurrentSafeModeState();
-            
-            assert.equal(safeModeOn, true);
-            assert.equal(safeModeOff, false);
+          await owned.setSafeMode(true);
+          const safeModeOn: boolean = await owned.getCurrentSafeModeState();
+          await owned.setSafeMode(false);
+          const safeModeOff: boolean = await owned.getCurrentSafeModeState();
+
+          assert.equal(safeModeOn, true);
+          assert.equal(safeModeOff, false);
         });
 
         it("reverts if you are not a deployer", async () => {
-            await expect(
-                owned.connect(accounts[2]).setSafeMode(true)
-            ).revertedWith("You have to be a deployer to do this");
+          await expect(
+            owned.connect(accounts[2]).setSafeMode(true)
+          ).revertedWithCustomError(owned, "Unauthorized");
         });
-    });
+      });
 
-    describe("addDeployer", function(){
-        it("adds a new deployer",async () => {
-            await owned.addDeployer(accounts[2].address);
-            const isDeployer: boolean = await owned.isDeployer(accounts[2].address);
+      describe("addDeployer", function () {
+        it("adds a new deployer", async () => {
+          await owned.addDeployer(accounts[2].address);
+          const isDeployer: boolean = await owned.isDeployer(
+            accounts[2].address
+          );
 
-            assert.equal(isDeployer, true);
+          assert.equal(isDeployer, true);
         });
 
         it("reverts if address is already a deployer", async () => {
-            await owned.addDeployer(accounts[2].address);
+          await owned.addDeployer(accounts[2].address);
 
-            await expect(
-                owned.addDeployer(accounts[2].address)
-            ).revertedWith("The specified address is already a deployer");
+          await expect(
+            owned.addDeployer(accounts[2].address)
+          ).revertedWithCustomError(owned, "AlreadyADeployer");
         });
 
         it("reverts if you are not a deployer", async () => {
-            await expect(
-                owned.connect(accounts[2]).addDeployer(accounts[2].address)
-            ).revertedWith("You have to be a deployer to do this");
+          await expect(
+            owned.connect(accounts[2]).addDeployer(accounts[2].address)
+          ).revertedWithCustomError(owned, "Unauthorized");
         });
-    });
+      });
 
-    describe("deleteDeployer", function(){
+      describe("deleteDeployer", function () {
         it("deletes a deployer", async () => {
-            await owned.deleteDeployer(secondDeployer.address);
-            const isDeployer: boolean = await owned.isDeployer(secondDeployer.address);
+          await owned.deleteDeployer(secondDeployer.address);
+          const isDeployer: boolean = await owned.isDeployer(
+            secondDeployer.address
+          );
 
-            assert.equal(isDeployer, false);
+          assert.equal(isDeployer, false);
         });
 
         it("reverts is address is not a deployer", async () => {
-            await expect(
-                owned.deleteDeployer(accounts[2].address)
-            ).revertedWith("The specified address is not a deployer");
+          await expect(
+            owned.deleteDeployer(accounts[2].address)
+          ).revertedWithCustomError(owned, "NotADeployer");
         });
 
         it("reverts if you are not a deployer", async () => {
-            await expect(
-                owned.connect(accounts[2]).addDeployer(secondDeployer.address)
-            ).revertedWith("You have to be a deployer to do this");
+          await expect(
+            owned.connect(accounts[2]).addDeployer(secondDeployer.address)
+          ).revertedWithCustomError(owned, "Unauthorized");
         });
-    })
-});
+      });
+    });
