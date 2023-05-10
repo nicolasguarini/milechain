835f31eda856ea7791fbf1a6069b6d1f5f48dba7
Zeref-zt <lorenzo.ficazzola@outlook.it>
2023-05-10 11:40:12 +0200

    Add update data owner in to the db

--- frontend/utils/createToken.ts
+++ frontend/utils/createToken.ts
@@ -1,5 +1,5 @@
 
-export function createToken() {
+export function createToken(address: string) {
     var SHA256 = require("crypto-js/sha256");
-    return SHA256(process.env.NEXT_PUBLIC_SECRET).toString();
+    return SHA256(address+process.env.NEXT_PUBLIC_SECRET).toString();
 }
