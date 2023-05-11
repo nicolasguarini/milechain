369e39843c1ac813ad9ff85f0c8ff074c31a2bd4
Nicolas Guarini <nikiguarincia@gmail.com>
2023-05-12 01:16:27 +0200

    Redeploy localhost

--- backend/constants/chains.ts
+++ backend/constants/chains.ts
@@ -1,1 +1,4 @@
-export const chainsMap: Map<number, string> = new Map([[11155111, "sepolia"]]);
+export const chainsMap: Map<number, string> = new Map([
+  [11155111, "sepolia"],
+  [31337, "localhost"],
+]);
