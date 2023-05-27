465be46d79f0e41a0953f0a62fbc390e531600c4
Nicolas Guarini <nikiguarincia@gmail.com>
2023-05-27 10:45:47 +0200

    Improve user experience during advanced search

--- frontend/components/spinner.tsx
+++ frontend/components/spinner.tsx
@@ -1,5 +1,5 @@
 export default function Spinner() {
   return (
-    <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full self-center"></div>
+    <div className="animate-spin spinner-border m-auto h-8 w-8 border-b-2 rounded-full self-center"></div>
   );
 }
--- frontend/pages/advanced-search.tsx
+++ frontend/pages/advanced-search.tsx
@@ -5,6 +5,7 @@ import { Notify } from "notiflix";
 import { FormEvent, useState } from "react";
 import { useMoralis } from "react-moralis";
 import Link from "next/link";
+import Spinner from "@/components/spinner";
 
 interface Vehicle {
   licensePlate: string;
@@ -16,13 +17,16 @@ export default function AdvancedSearchPage() {
   const baseUrl = process.env.NEXT_PUBLIC_SERVER_BASE_URL;
   const { chainId: chainIdHex } = useMoralis();
   const [licensePlate, setLicensePlate] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoadingSearch, setIsLoadingSearch] = useState(false);
+  const [isLoadingUpdate, setIsLoadingUpdate] = useState(false);
   const chainId: number = chainIdHex ? parseInt(chainIdHex) : defaultChain;
   const networkName = chainsMap.get(chainId);
   const [vehicle, setVehicle] = useState<Vehicle | null>(null);
+  const [showResults, setShowResults] = useState(false);
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    setIsLoadingSearch(true);
     const url = `${baseUrl}getVehicle?chainId=${chainId}&query=${licensePlate}`;
 
     fetch(url, { method: "GET" })
@@ -32,29 +36,39 @@ export default function AdvancedSearchPage() {
           Notify.failure("Please connect to a valid network");
         } else {
           setVehicle(data.vehicle);
+          setIsLoadingSearch(false);
         }
       });
+
+    setShowResults(true);
   };
 
   const handleCheckBlockchainClick = () => {
-    const url = `${baseUrl}updateVehicle?chainId=${chainId}&licensePlate=${licensePlate}`;
-    fetch(url, { method: "GET" })
-      .then((res) => {
-        console.log("Response code: " + res.status.toString());
-        res.json().then((data) => {
-          Notify.info(data.message);
-          if (data.vehicle != undefined && data.vehicle != null) {
-            setVehicle({
-              licensePlate: data.vehicle[0],
-              owner: data.vehicle[1],
-              mileage: data.vehicle[2],
-            });
-          }
+    if (!networkName) {
+      Notify.failure("Please connect to a valid network!");
+    } else {
+      setIsLoadingUpdate(true);
+      const url = `${baseUrl}updateVehicle?chainId=${chainId}&licensePlate=${licensePlate}`;
+      fetch(url, { method: "GET" })
+        .then((res) => {
+          console.log("Response code: " + res.status.toString());
+          res.json().then((data) => {
+            Notify.info(data.message);
+            setIsLoadingUpdate(false);
+            if (data.vehicle != undefined && data.vehicle != null) {
+              setVehicle({
+                licensePlate: data.vehicle[0],
+                owner: data.vehicle[1],
+                mileage: data.vehicle[2],
+              });
+            }
+          });
+        })
+        .catch((err) => {
+          Notify.failure(err);
+          setIsLoadingUpdate(false);
         });
-      })
-      .catch((err) => {
-        Notify.failure(err);
-      });
+    }
   };
 
   return (
@@ -62,11 +76,10 @@ export default function AdvancedSearchPage() {
       <Container>
         <div className="flex flex-col items-center py-12">
           <h1 className="text-4xl font-bold my-3">Advanced Search</h1>
-          <p className="my-6 text-center">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente
-            totam quisquam minus. Consectetur veniam at necessitatibus
-            cupiditate corporis, nemo illo, maxime, nostrum aliquid provident
-            vero obcaecati ullam nesciunt id fuga?
+          <p className="my-6 text-center max-w-3xl">
+            Advanced search allows you to search or update vehicle data by
+            directly contacting the blockchain. If more updated data is found
+            the database will be updated automatically.
           </p>
         </div>
         <form className="w-full mb-5" onSubmit={handleSubmit}>
@@ -106,41 +119,76 @@ export default function AdvancedSearchPage() {
         </form>
 
         <div>
-          {vehicle ? (
-            
-             <div className="flex flex-row justify-between items-center">
-                      <div className="items-center">
-                        <h1 className="text-4xl font-bold border-b-2 pb-1 border-accent w-fit m-auto">
-                          <Link href={`/vehicles/${vehicle.licensePlate}`}>
-                            {vehicle.licensePlate}
-                          </Link>
-                        </h1>
-                      </div>
-                      <div>
-                      <button className="block" onClick={handleCheckBlockchainClick}>
-                          Update from blockchain
-                        </button>
-                      </div>
-                      <div className="flex flex-col items-right">
-                        <p className="text-xl text-right">
-                          Current Mileage: {vehicle.mileage} km
-                        </p>
-                        <p className="text-xl text-right border-b-2">
-                          <Link href={`/owners/${vehicle.owner}`}>
-                          Owned by:{vehicle.owner.substring(0,6)+"..."+vehicle.owner.substring(vehicle.owner.length-5,vehicle.owner.length)}
-                          </Link>
-                        </p>
-                       
-                      </div>
-                    </div>
-             
-          ) : (
-            <div className=" flex flex-col items-center text-xl font-bold">
-              <h3>Vehicle Not Found in db</h3>
-              <button className="border block border-accent px-9 py-2 rounded-full mt-10" onClick={handleCheckBlockchainClick}>
-                Check on blockchain
-              </button>
+          {isLoadingSearch ? (
+            <div className="w-fit m-auto mt-5">
+              <Spinner />
             </div>
+          ) : (
+            <>
+              {vehicle ? (
+                <div className="flex flex-row justify-between items-center pt-12">
+                  <div className="items-center">
+                    <h1 className="text-4xl font-bold border-b-2 pb-1 border-accent w-fit m-auto">
+                      <Link href={`/vehicles/${vehicle.licensePlate}`}>
+                        {vehicle.licensePlate}
+                      </Link>
+                    </h1>
+                  </div>
+                  <div className="flex flex-col items-right">
+                    <p className="text-xl text-center">{vehicle.mileage} km</p>
+                    <p className="text-xl text-right">
+                      <Link href={`/owners/${vehicle.owner}`}>
+                        Owned by{" "}
+                        <span className="underline">
+                          {vehicle.owner.substring(0, 6) +
+                            "..." +
+                            vehicle.owner.substring(
+                              vehicle.owner.length - 5,
+                              vehicle.owner.length
+                            )}
+                        </span>
+                      </Link>
+                    </p>
+                  </div>
+                  <div>
+                    {isLoadingUpdate ? (
+                      <div className="block w-60">
+                        <Spinner />
+                      </div>
+                    ) : (
+                      <button
+                        className="border block border-accent px-9 py-2 rounded-full"
+                        onClick={handleCheckBlockchainClick}
+                      >
+                        Update from blockchain
+                      </button>
+                    )}
+                  </div>
+                </div>
+              ) : (
+                <>
+                  {showResults ? (
+                    <div className=" flex flex-col items-center">
+                      <h3 className="text-xl font-bold pt-3">
+                        Vehicle Not Found
+                      </h3>
+                      {isLoadingUpdate ? (
+                        <span className="mt-7">
+                          <Spinner />
+                        </span>
+                      ) : (
+                        <button
+                          className="border block border-accent px-9 py-2 rounded-full mt-6"
+                          onClick={handleCheckBlockchainClick}
+                        >
+                          Check on blockchain
+                        </button>
+                      )}
+                    </div>
+                  ) : null}
+                </>
+              )}
+            </>
           )}
         </div>
       </Container>
